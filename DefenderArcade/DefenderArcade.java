5987a76c2d5c6aead07c7b972bed73e3d58b06d3
luckchrist1993 <85020629+luckchrist1993@users.noreply.github.com>
2021-05-29 19:07:55 +0700

    DefenderArcade

--- DefenderArcade/DefenderArcade.java
+++ DefenderArcade/DefenderArcade.java
@@ -1,7 +1,35 @@
 import java.util.List;
 
 public class DefenderArcade {
-  public int countArcades(List<String> times) {
-    return 0;
-  }
+	
+	public String setTime(String time) {
+		if (time.length() == 3) {
+			time = "0" + time;
+		}
+		time = time.substring(0, 2) + ":" + time.substring(2, 4);
+		return time;
+	}
+	
+	public int countArcades(List<String> times) {
+		String lastTime = "";
+		int result = 0;
+		for (int i = 0; i < times.size(); i++) {
+			String[] range = times.get(i).split(" ");
+			if (i > 0) {
+				String firstTime = setTime(range[0]);
+				
+				String[] splitLastTime = lastTime.split(":");
+				String[] splitFirstTime = firstTime.split(":");
+				
+				int minuteLastTime = Integer.parseInt(splitLastTime[0]) * 60 + Integer.parseInt(splitLastTime[1]);
+				int minuteFirstTime = Integer.parseInt(splitFirstTime[0]) * 60 + Integer.parseInt(splitFirstTime[1]);
+				
+				if (minuteLastTime > minuteFirstTime) {
+					result++;
+				}
+			}
+			lastTime = setTime(range[1]);
+		}
+		return result;
+	}
 }
